0aa1e9edb09f8781443464b1162a7e959dc3e038
Darren Odden <darren@odden.io>
2018-05-27 19:09:26 -0700

    initial flamelink included

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Flamelink from 'flamelink'
+
 import App from './App'
 import router from './router'
+
+const app = Flamelink({
+  apiKey: 'AIzaSyAfAryrFwdxYAhkGLlvPi9Zmw29aNRHVJk',
+  authDomain: 'darren-by-design.firebaseapp.com',
+  databaseURL: 'https://darren-by-design.firebaseio.com',
+  projectId: 'darren-by-design',
+  storageBucket: 'darren-by-design.appspot.com',
+  messagingSenderId: '198481969014'
+})
+
+app.content.get('posts')
+  .then(posts => console.log('All posts: ', posts))
+  .catch(error => console.log('Something gone wrong', error))
 
 Vue.config.productionTip = false
 
