900d89d05320ffe2855b3d2fb9fb5dbc52baeecf
Darren Odden <darren@odden.io>
2018-06-04 08:52:31 -0700

    hero

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hero">
+    <h1>{{ hero }}</h1>
+  </div>
+</template>
+
+<script>
+import { title } from '@/main'
+// import { postTitle } from '@/components/Writ'
+export default {
+  name: 'Hero',
+  data () {
+    return {
+      hero: title
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
